0056e263bded917445a584277555cb90edd90215
Suraj <surajsngbasnal16@gmail.com>
2023-10-07 21:14:10 +0530

    Second commit

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ function cartPage() {
     cross.addEventListener('click', function () {
         shop.classList.remove('hide');
         cross.classList.add('hide');
-        cart.style.top = "-150%"
+        cart.style.top = "-200%"
     });
 }
 cartPage()
